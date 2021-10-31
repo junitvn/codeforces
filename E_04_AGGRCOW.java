152abc983df2ecd651f224d71aebdfd951e51185
Lam Nguyen <lamnn.dev@mail.com>
2021-11-01 06:21:47 +0700

    add binary search cows

--- E_04_AGGRCOW.java
+++ E_04_AGGRCOW.java
@@ -8,25 +8,32 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class E_04_AGGRCOW {
+    static long res = Integer.MAX_VALUE;
 
-    static long bs(int n, int c, long[] T) {
+    static long bs(int n, int c, int[] T) {
         long l = -1;
         long r = T[T.length - 1];
         while (r - l > 1) {
             long mid = (r + l) / 2;
             if (check(c, n, mid, T)) {
-                l = mid;
-            } else {
                 r = mid;
+            } else {
+                l = mid;
             }
         }
-        return r;
+        return l;
     }
 
-    static boolean check(int c, int n, long mid, long[] T) {
-        int count = 0;
-
-        return count == c;
+    static boolean check(int c, int n, long mid, int[] T) {
+        int count = 1;
+        int i = 1;
+        for (int pos = 1; pos <= n; pos++) {
+            if (T[pos] - T[i] >= mid) {
+                count++;
+                i = pos;
+            }
+        }
+        return count < c;
     }
 
     public static void main(String[] args) throws IOException {
@@ -38,16 +45,16 @@ public class E_04_AGGRCOW {
             int n = Integer.parseInt(ncLine[0]); // number of stalls
             int c = Integer.parseInt(ncLine[1]); // number of cows
             int[] stallPosition = new int[n + 1];
-            long[] T = new long[n + 1];
             for (int i = 1; i <= n; i++) {
                 String line[] = br.readLine().split(" ");
                 stallPosition[i] = Integer.parseInt(line[0]);
             }
             Arrays.sort(stallPosition);
-            for (int i = 1; i < T.length; i++) {
-                T[i] = T[i - 1] + stallPosition[i];
-            }
-
+            // for (int i = 1; i < T.length; i++) {
+            // T[i] = T[i - 1] + stallPosition[i];
+            // }
+            res = bs(n, c, stallPosition);
+            System.out.println(res);
         }
     }
 }
